562d33cc85edc84849c00b2df7f75344daaa099e
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-06-20 23:27:44 -0300

    add prisma

---
+++ bonus/src/components/TaskItem.tsx
@@ -0,0 +1,48 @@
+"use-client";
+
+import { Task } from "@/interfaces/task.interface";
+import Image from "next/image";
+
+export const TaskItem = ({ task }: { task: Task }) => {
+  // const { removeTask, toggleTask } = useCont11ext(TaskContext);
+
+  // const navigate = useNavigate();
+
+  return (
+    <li
+      key={task.id}
+      className="flex   items-center gap-4 p-[1rem] border-b-1 "
+    >
+      <button
+        className={`${task.completed ? "check-button" : "uncheck-button"} `}
+      >
+        <Image
+          src={"/assets/images/icon-check.svg"}
+          width={16}
+          height={16}
+          alt="check"
+        />
+      </button>
+
+      <p className="grow-1">{task.title}</p>
+      <div className="flex gap-3 items-center">
+        <button>
+          <Image
+            src={"/assets/images/icon-edit.svg"}
+            width={16}
+            height={16}
+            alt="edit"
+          />
+        </button>
+        <button>
+          <Image
+            width={16}
+            height={16}
+            src={"/assets/images/icon-cross.svg"}
+            alt="cross-delete"
+          />
+        </button>
+      </div>
+    </li>
+  );
+};
---
+++ bonus/src/components/TaskList.tsx
@@ -0,0 +1,27 @@
+"use-client";
+import { Task } from "@/interfaces/task.interface";
+import { TaskItem } from "./TaskItem";
+
+interface Props {
+  tasks: Task[];
+  loading: boolean;
+}
+
+export const TaskList = ({ tasks, loading }: Props) => {
+  return (
+    <>
+      <ul>
+        {loading ? (
+          <div className="loading">
+            <div className="spinner"></div>
+          </div>
+        ) : (
+          tasks.map((task: Task) => <TaskItem key={task.id} task={task} />)
+        )}
+      </ul>
+      <div className="flex justify-center items-center p-[1rem]">
+        <p>{tasks.length} items</p>
+      </div>
+    </>
+  );
+};
